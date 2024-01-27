112e9e7f100bf6d0b55061b803bb489209c9b593
mhayeri <hayerimustafa@gmail.com>
2024-01-27 15:38:51 -0800

    Defined urls needed to fetch data from using TMDb API

---
+++ src/requests/Requests.js
@@ -0,0 +1,12 @@
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const requests = {
+    TRENDING: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    TOP_RATED: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    ACTION: `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    COMEDY: `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    HORROR: `${BASE_URL}/search/movie?api_key=${API_KEY}&with_genres=27`,
+};
+
+export default requests;
