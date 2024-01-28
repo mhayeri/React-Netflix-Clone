36269b13f976c64ef74c61c09e46adbcfb5e4e56
mhayeri <hayerimustafa@gmail.com>
2024-01-27 16:55:10 -0800

    Updated useEffect method to be async, added multiple checks for movie title

--- src/components/Backdrop.jsx
+++ src/components/Backdrop.jsx
@@ -15,9 +15,13 @@ const Backdrop = () => {
     };
 
     useEffect(() => {
-        axios.get(requests.TRENDING).then((response) => {
-            setMovies(response.data.results);
-        });
+        async function fetchMovies() {
+            const request = await axios.get(requests.TRENDING);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchMovies();
     }, []);
 
     return (
@@ -26,12 +30,22 @@ const Backdrop = () => {
                 <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
                 <img
                     className="w-full h-full object-cover"
-                    src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-                    alt={movie?.title}
+                    src={`https://image.tmdb.org/t/p/original/${
+                        movie?.backdrop_path || movie?.poster_path
+                    }`}
+                    alt={
+                        movie?.title ||
+                        movie?.original_title ||
+                        movie?.name ||
+                        movie?.original_name
+                    }
                 />
                 <div className="absolute w-full top-[20%] p-4 md:p-8">
                     <h1 className="text-3xl md:text-5xl font-bold">
-                        {movie?.title}
+                        {movie?.title ||
+                            movie?.original_title ||
+                            movie?.name ||
+                            movie?.original_name}
                     </h1>
                     <div className="my-4">
                         <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5 hover:scale-105 duration-300">
