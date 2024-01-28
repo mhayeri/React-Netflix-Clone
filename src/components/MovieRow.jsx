7fd5cae0f747d229c1b432363100c6f9066720dd
mhayeri <hayerimustafa@gmail.com>
2024-01-27 17:12:34 -0800

    Rendered new Movie component for retrieved movies, added styling to put all movies for each category in a row

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
 import axios from "axios";
+
+import Movie from "./Movie";
 
 const MovieRow = ({ title, url }) => {
     const [movies, setMovies] = useState([]);
-    const [like, setLike] = useState(false);
 
     useEffect(() => {
         async function fetchMovies() {
@@ -20,43 +20,12 @@ const MovieRow = ({ title, url }) => {
         <>
             <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
             <div className="relative flex items-center">
-                <div id={"slider"}>
+                <div
+                    id={"slider"}
+                    className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+                >
                     {movies.map((movie, id) => {
-                        return (
-                            <div
-                                key={id}
-                                className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
-                            >
-                                <img
-                                    className="w-full h-auto block"
-                                    src={`https://image.tmdb.org/t/p/w500/${
-                                        movie?.backdrop_path ||
-                                        movie?.poster_path
-                                    }`}
-                                    alt={
-                                        movie?.title ||
-                                        movie?.original_title ||
-                                        movie?.name ||
-                                        movie?.original_name
-                                    }
-                                />
-                                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                                    <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                                        {movie?.title ||
-                                            movie?.original_title ||
-                                            movie?.name ||
-                                            movie?.original_name}
-                                    </p>
-                                    <p>
-                                        {like ? (
-                                            <FaHeart className="absolute top-4 left-4 text-gray-300" />
-                                        ) : (
-                                            <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
-                                        )}
-                                    </p>
-                                </div>
-                            </div>
-                        );
+                        return <Movie key={id} movie={movie} />;
                     })}
                 </div>
             </div>
