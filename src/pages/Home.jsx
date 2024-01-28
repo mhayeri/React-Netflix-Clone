68f7eb7a095a68389cd2652daa646b6eab4d0e45
mhayeri <hayerimustafa@gmail.com>
2024-01-27 16:05:06 -0800

    Created home page to be used throughout app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Backdrop from "../components/Backdrop";
+
+const Home = () => {
+    return (
+        <>
+            <Backdrop />
+        </>
+    );
+};
+
+export default Home;
