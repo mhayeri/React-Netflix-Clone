d0b97b1578f9e9257a197bcfc3f0f9f82d14f2ae
mhayeri <hayerimustafa@gmail.com>
2024-01-28 21:10:23 -0800

    added conditional rendering to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+    const { user, logOut } = UserAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await logOut();
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
             <Link to="/">
@@ -9,19 +22,34 @@ const Navbar = () => {
                     NETFLIX
                 </h1>
             </Link>
-
-            <div className="">
-                <Link to="/login">
-                    <button className="text-white pr-4 rounded cursor-pointer hover:scale-105 duration-300 ">
-                        Sign In
+            {user?.email ? (
+                <div className="">
+                    <Link to="/account">
+                        <button className="text-white pr-4 rounded cursor-pointer hover:scale-105 duration-300 ">
+                            Account
+                        </button>
+                    </Link>
+                    <button
+                        className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer hover:scale-105 duration-300"
+                        onClick={handleLogout}
+                    >
+                        Logout
                     </button>
-                </Link>
-                <Link to="/signup">
-                    <button className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer hover:scale-105 duration-300">
-                        Sign Up
-                    </button>
-                </Link>
-            </div>
+                </div>
+            ) : (
+                <div className="">
+                    <Link to="/login">
+                        <button className="text-white pr-4 rounded cursor-pointer hover:scale-105 duration-300 ">
+                            Sign In
+                        </button>
+                    </Link>
+                    <Link to="/signup">
+                        <button className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer hover:scale-105 duration-300">
+                            Sign Up
+                        </button>
+                    </Link>
+                </div>
+            )}
         </div>
     );
 };
