6bf32f9d81ea5b9dfdde8fd6a119806eb27acd20
mhayeri <hayerimustafa@gmail.com>
2024-01-28 19:15:11 -0800

    Added links to the sign in/sign up pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
-            <h1 className="text-red-600 font-bold text-4xl cursor-pointer">
-                NETFLIX
-            </h1>
+            <Link to="/">
+                <h1 className="text-red-600 font-bold text-4xl cursor-pointer">
+                    NETFLIX
+                </h1>
+            </Link>
+
             <div className="">
-                <button className="text-white pr-4 rounded cursor-pointer hover:scale-105 duration-300 ">
-                    Sign In
-                </button>
-                <button className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer hover:scale-105 duration-300">
-                    Sign Up
-                </button>
+                <Link to="/login">
+                    <button className="text-white pr-4 rounded cursor-pointer hover:scale-105 duration-300 ">
+                        Sign In
+                    </button>
+                </Link>
+                <Link to="/signup">
+                    <button className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer hover:scale-105 duration-300">
+                        Sign Up
+                    </button>
+                </Link>
             </div>
         </div>
     );
