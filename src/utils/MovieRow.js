0aeb711a6b0d069802067e27fa7dbca7b0f9f14d
mhayeri <hayerimustafa@gmail.com>
2024-01-28 17:58:48 -0800

    Added id field to each object

--- src/utils/MovieRow.js
+++ src/utils/MovieRow.js
@@ -2,22 +2,27 @@ import requests from "../requests/Requests";
 
 export const movieRows = [
     {
+        id: 1,
         title: "Trending",
         url: requests.TRENDING,
     },
     {
+        id: 2,
         title: "Top Rated",
         url: requests.TOP_RATED,
     },
     {
+        id: 3,
         title: "Action",
         url: requests.ACTION,
     },
     {
+        id: 4,
         title: "Comedy",
         url: requests.COMEDY,
     },
     {
+        id: 5,
         title: "Horror",
         url: requests.HORROR,
     },
